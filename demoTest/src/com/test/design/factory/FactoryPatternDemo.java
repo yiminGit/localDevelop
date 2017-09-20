3925a60e4c519888757cfd4714833c98425aa16d
Bryant <yi.min.bryant@gmail.com>
2017-09-20 18:48:54 +0800

    add comment

--- demoTest/src/com/test/design/factory/FactoryPatternDemo.java
+++ demoTest/src/com/test/design/factory/FactoryPatternDemo.java
@@ -1,17 +1,44 @@
 package com.test.design.factory;
 
+/**
+ * 优点：1、一个调用者想创建一个对象，只要知道其名称就可以了。 
+ *     2、扩展性高，如果想增加一个产品，只要扩展一个工厂类就可以。 
+ *     3、屏蔽产品的具体实现，调用者只关心产品的接口。
+ * 缺点：每次增加一个产品时，都需要增加一个具体类和对象实现工厂，使得系统中类的个数成倍增加，在一定程度上增加了系统的复杂度，同时也增加了系统具体类的依赖。这并不是什么好事。
+ * 使用场景： 1、日志记录器：记录可能记录到本地硬盘、系统事件、远程服务器等，用户可以选择记录日志到什么地方。 
+ *        2、数据库访问，当用户不知道最后系统采用哪一类数据库，以及数据库可能有变化时。 
+ *        3、设计一个连接服务器的框架，需要三个协议，"POP3"、"IMAP"、"HTTP"，可以把这三个作为产品类，共同实现一个接口。
+ * @author Bryant
+ */
 public class FactoryPatternDemo {
 	
 	public static void main(String[] args) {
 		
+		ShapeFactory shapeFactory = new ShapeFactory();
+
+	    //获取 Circle 的对象，并调用它的 draw 方法
+	    Shape shape1 = shapeFactory.getShape("CIRCLE");
+	    //调用 Circle 的 draw 方法
+	    shape1.draw();
+
+	    //获取 Rectangle 的对象，并调用它的 draw 方法
+	    Shape shape2 = shapeFactory.getShape("RECTANGLE");
+	    //调用 Rectangle 的 draw 方法
+	    shape2.draw();
+
+	    //获取 Square 的对象，并调用它的 draw 方法
+	    Shape shape3 = shapeFactory.getShape("SQUARE");
+	    //调用 Square 的 draw 方法
+	    shape3.draw();
+	    
+		/*
 		Rectangle rect = (Rectangle) ShapeFactory.getClass(Rectangle.class);
 		rect.draw();
-		
 		Square square = (Square) ShapeFactory.getClass(Square.class);
 		square.draw();
-		
 		Circle circle = (Circle) ShapeFactory.getClass(Circle.class);
 		circle.draw();
+		*/
 	}
 
 }
