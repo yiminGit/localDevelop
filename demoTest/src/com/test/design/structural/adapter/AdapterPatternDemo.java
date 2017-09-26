9403da1a3eb035f75b308c13d997b660952cdcbe
Bryant <yi.min.bryant@gmail.com>
2017-09-26 17:33:29 +0800

    AdapterPatternDemo complete

---
+++ demoTest/src/com/test/design/structural/adapter/AdapterPatternDemo.java
@@ -0,0 +1,21 @@
+package com.test.design.structural.adapter;
+
+/**
+ * 主要解决：主要解决在软件系统中，常常要将一些"现存的对象"放到新的环境中，而新环境要求的接口是现对象不能满足的。
+ * @author Bryant
+ *
+ */
+public class AdapterPatternDemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		AudioPlayer audioPlayer = new AudioPlayer();
+
+		audioPlayer.play("mp3", "beyond the horizon.mp3");
+		audioPlayer.play("mp4", "alone.mp4");
+		audioPlayer.play("vlc", "far far away.vlc");
+		audioPlayer.play("avi", "mind me.avi");
+	}
+
+}
---
+++ demoTest/src/com/test/design/structural/adapter/MediaPlayer.java
@@ -0,0 +1,6 @@
+package com.test.design.structural.adapter;
+
+public interface MediaPlayer {
+
+	public void play(String audioType, String fileName);
+}
