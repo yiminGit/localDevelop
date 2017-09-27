cd783e1b9c90572d76968597aaa2ef33a6a8945b
Bryant <yi.min.bryant@gmail.com>
2017-09-27 15:43:55 +0800

    BridgePatternDemo complete

---
+++ demoTest/src/com/test/design/structural/bridge/Shape.java
@@ -0,0 +1,12 @@
+package com.test.design.structural.bridge;
+
+public abstract class Shape {
+
+	protected DrawAPI drawAPI;
+
+	protected Shape(DrawAPI drawAPI) {
+		this.drawAPI = drawAPI;
+	}
+
+	public abstract void draw();
+}
