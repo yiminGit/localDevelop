fd20ae4fc32dea8251b475e51471fb46bae23516
Bryant <yi.min.bryant@gmail.com>
2017-10-12 10:23:04 +0800

    FacadePatternDemo complete

---
+++ demoTest/src/com/test/design/structural/facade/Circle.java
@@ -0,0 +1,11 @@
+package com.test.design.structural.facade;
+
+public class Circle implements Shape{
+
+	@Override
+	public void draw() {
+		// TODO Auto-generated method stub
+		System.out.println("Circle::draw()");
+	}
+
+}
---
+++ demoTest/src/com/test/design/structural/facade/Shape.java
@@ -0,0 +1,5 @@
+package com.test.design.structural.facade;
+
+public interface Shape {
+	void draw();
+}
