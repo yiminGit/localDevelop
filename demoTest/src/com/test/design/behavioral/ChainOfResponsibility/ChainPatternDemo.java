dcce12ee7b1b27e5cae632015bb07b76c561acb3
Bryant <yi.min.bryant@gmail.com>
2017-11-22 11:51:47 +0800

    chain pattern

---
+++ demoTest/src/com/test/design/behavioral/ChainOfResponsibility/ChainPatternDemo.java
@@ -0,0 +1,33 @@
+package com.test.design.behavioral.ChainOfResponsibility;
+
+/**
+ * 使用场景： 1、有多个对象可以处理同一个请求，具体哪个对象处理该请求由运行时刻自动确定。 
+ *        2、在不明确指定接收者的情况下，向多个对象中的一个提交一个请求。
+ *        3、可动态指定一组对象处理请求。
+ * @author Bryant
+ *
+ */
+public class ChainPatternDemo {
+
+	private static AbstractLogger getChainOfLoggers() {
+
+		AbstractLogger errorLogger = new ErrorLogger(AbstractLogger.ERROR);
+		AbstractLogger fileLogger = new FileLogger(AbstractLogger.DEBUG);
+		AbstractLogger consoleLogger = new ConsoleLogger(AbstractLogger.INFO);
+
+		errorLogger.setNextLogger(fileLogger);
+		fileLogger.setNextLogger(consoleLogger);
+
+		return errorLogger;
+	}
+
+	public static void main(String[] args) {
+		AbstractLogger loggerChain = getChainOfLoggers();
+
+		loggerChain.logMessage(AbstractLogger.INFO, "This is an information.");
+
+		loggerChain.logMessage(AbstractLogger.DEBUG, "This is an debug level information.");
+
+		loggerChain.logMessage(AbstractLogger.ERROR, "This is an error information.");
+	}
+}
---
+++ demoTest/src/com/test/design/behavioral/ChainOfResponsibility/FileLogger.java
@@ -0,0 +1,13 @@
+package com.test.design.behavioral.ChainOfResponsibility;
+
+public class FileLogger extends AbstractLogger {
+
+	public FileLogger(int level) {
+		this.level = level;
+	}
+
+	@Override
+	protected void write(String message) {
+		System.out.println("File::Logger: " + message);
+	}
+}
