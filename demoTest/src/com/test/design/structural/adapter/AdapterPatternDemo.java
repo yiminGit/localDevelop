580cd8aa55b3eea7ed5ef52db1a4d1a05c0b06a7
Bryant <yi.min.bryant@gmail.com>
2017-11-22 11:51:31 +0800

    modify

--- demoTest/src/com/test/design/structural/adapter/AdapterPatternDemo.java
+++ demoTest/src/com/test/design/structural/adapter/AdapterPatternDemo.java
@@ -2,6 +2,11 @@ package com.test.design.structural.adapter;
 
 /**
  * 主要解决：主要解决在软件系统中，常常要将一些"现存的对象"放到新的环境中，而新环境要求的接口是现对象不能满足的。
+ * 优点： 1、可以让任何两个没有关联的类一起运行。 (Mp4Player及VlcPlayer在MediaAdapter一起運行)
+ *     2、提高了类的复用。 
+ *     3、增加了类的透明度。 
+ *     4、灵活性好。
+ * 
  * @author Bryant
  *
  */
